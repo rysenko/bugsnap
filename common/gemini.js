7cc1bd751af18f6b3eb306cf913627d6ad08dd89
arysenko <devnull@localhost>
2013-04-24 10:54:37 +0300

    Fix issue 21 "Capture isn't aware of the options change"

--- common/gemini.js
+++ common/gemini.js
@@ -1,17 +1,16 @@
 define(['js/jquery'], function ($) {
     var GeminiCommunicator = (function () {
         function GeminiCommunicator() {
-            if(window.navigator.userAgent.indexOf('Firefox') != -1) { // add options page for FF later
-                this.geminiUrl = "http://rysenkocomp.dlinkddns.com/gemini/api/";
-                this.geminiUsername = window.btoa('manager:qjbpnsqg6i'); // user:apikey
-            } else {
-                this.geminiUrl = localStorage["GeminiUrl"]+ "/api/";
-                this.geminiUsername = window.btoa(localStorage["UserName"] + ':' + localStorage["APIKey"]); // user:apikey
-            }
+            this.geminiUrl = function () {
+                return localStorage["GeminiUrl"]+ "/api/";
+            };
+            this.geminiUsername = function () {
+                return window.btoa(localStorage["UserName"] + ':' + localStorage["APIKey"]);
+            };
         }
         GeminiCommunicator.prototype.search = function (query) {
            var options = {
-                url: this.geminiUrl + "items/filtered",
+                url: this.geminiUrl() + "items/filtered",
                 type: "POST",
                 data: {
                     SearchKeywords: query,
@@ -19,10 +18,10 @@ define(['js/jquery'], function ($) {
                     Projects: "ALL",
 					MaxItemsToReturn: 10
                 },
-                headers: { "Authorization": "Basic " + this.geminiUsername }
+                headers: { "Authorization": "Basic " + this.geminiUsername() }
             };
 			if(!isNaN(query)) {
-				options.url = this.geminiUrl + "items/" + query;
+				options.url = this.geminiUrl() + "items/" + query;
 				options.type = "GET";
 				options.data = null;
 			}
@@ -33,7 +32,7 @@ define(['js/jquery'], function ($) {
         };
         GeminiCommunicator.prototype.comment = function (projectId, issueId, comment) {
             return $.ajax({
-                url: this.geminiUrl + "items/" + issueId + "/comments",
+                url: this.geminiUrl() + "items/" + issueId + "/comments",
                 type: "POST",
                 data: {
                     ProjectId: projectId,
@@ -41,12 +40,12 @@ define(['js/jquery'], function ($) {
                     UserId: "1",
                     Comment: comment
                 },
-                headers: { "Authorization": "Basic " + this.geminiUsername }
+                headers: { "Authorization": "Basic " + this.geminiUsername() }
             });
         };
         GeminiCommunicator.prototype.attach = function (projectId, issueId, fileContent) {
             return $.ajax({
-                url: this.geminiUrl + "items/" + issueId + "/attachments",
+                url: this.geminiUrl() + "items/" + issueId + "/attachments",
                 type: "POST",
                 data: JSON.stringify({
                     ProjectId: projectId,
@@ -57,7 +56,7 @@ define(['js/jquery'], function ($) {
                 }),
                 processData: false,
                 contentType: 'application/json',
-                headers: { "Authorization": "Basic " + this.geminiUsername }
+                headers: { "Authorization": "Basic " + this.geminiUsername() }
             });
         };
         return GeminiCommunicator;
