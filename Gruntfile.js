96479e0b4d815e0c4e28f39c4419731690b7aac0
rysenko <devnull@localhost>
2013-07-01 11:40:04 +0300

    Working on Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,34 @@
 module.exports = function(grunt) {
-  var path = require('path');
-  grunt.initConfig({
-    symlink: {
-      chrome: {
-        target: path.resolve('common'),
-        link: path.resolve('chrome/common')
-      },
-      firefox: {
-        target: path.resolve('common'),
-        link: path.resolve('firefox/resources/bugsnap/data/common')
-      }
-    },
-    clean: ['chrome/common', 'firefox/resources/bugsnap/data/common'],
-    crx: {
-      myPublicPackage: {
-        "src": "chrome/",
-        "dest": "."
-      }
-    }
-  });
+    var path = require('path');
+    grunt.initConfig({
+        symlink: {
+            chrome: {
+                target: path.resolve('common'),
+                link: path.resolve('chrome/common')
+            },
+            firefox: {
+                target: path.resolve('common'),
+                link: path.resolve('firefox/resources/bugsnap/data/common')
+            }
+        },
+        clean: ['chrome/common', 'firefox/resources/bugsnap/data/common'],
+        crx: {
+            bugsnap: {
+                "src": "chrome/",
+                "dest": "build/chrome.crx"
+            }
+        },
+        zip: {
+            'build/firefox.xpi': ['firefox/*']
+        }
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-symlink');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-crx');
+    grunt.loadNpmTasks('grunt-contrib-symlink');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-crx');
+    grunt.loadNpmTasks('grunt-zip');
 
-  grunt.registerTask('default', ['crx']);
-  grunt.registerTask('install', ['symlink']);
-  grunt.registerTask('uninstall', ['clean']);
+    grunt.registerTask('default', ['crx', 'zip']);
+    grunt.registerTask('install', ['symlink']);
+    grunt.registerTask('uninstall', ['clean']);
 };
