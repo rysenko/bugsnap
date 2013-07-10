08be8111a05dfabc4aa1ab4ab19495cdac6afb01
stealth <stealth@1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.ip6.arpa>
2013-07-10 08:47:13 +0300

    Added Rally option

--- chrome/common/js/comm.js
+++ chrome/common/js/comm.js
@@ -1,10 +1,14 @@
-define(['comm/gemini', 'comm/youtrack', 'comm/fieldInfo'], function (GeminiCommunicator, YouTrackCommunicator) {
+define(['comm/gemini', 'comm/youtrack', 'comm/rally', 'comm/fieldInfo'],
+    function (GeminiCommunicator, YouTrackCommunicator, RallyCommunicator) {
     var CommunicatorLoader = function (communicatorType) {
         var type = communicatorType || localStorage['CommunicatorType'];
         var result = GeminiCommunicator; // Default one
         switch (type) {
             case 'YouTrack':
                 result = YouTrackCommunicator;
+                break;
+            case 'Rally':
+                result = RallyCommunicator;
                 break;
         }
         return result;
