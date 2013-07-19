0aa9cb0a535fc36571f0ec8bbe5744a4b1de4789
stealth <stealth@stealthmac.local>
2013-07-19 23:58:42 +0300

    Fixes #65: Support for Jira

--- chrome/common/js/comm/jira.js
+++ chrome/common/js/comm/jira.js
@@ -10,10 +10,8 @@ define(['lib/jquery', 'comm/communicator', 'comm/fieldInfo'], function ($, Commu
                 }
                 return url;
             };
+            this.MetaData = {projects: []};
         }
-        JiraCommunicator.prototype.authenticate = function () {
-            return this.ajax(this.Url() + 'rest/user/login', {login: this.Login(), password: this.Password()});
-        };
         JiraCommunicator.prototype.test = function () {
             return this.loadProjects();
         };
@@ -34,24 +32,41 @@ define(['lib/jquery', 'comm/communicator', 'comm/fieldInfo'], function ($, Commu
         };
         JiraCommunicator.prototype.getFields = function () {
             var fields = {
-                project: new FieldInfo({Caption: 'Project'})
+                project: new FieldInfo({Caption: 'Project'}),
+                issuetype: new FieldInfo({Caption: 'Issue Type'})
             };
             var self = this;
-            this.loadProjects().done(function (data) {
-                fields.project.Options(data);
+            this.ajax(this.Url() + 'rest/api/2/issue/createmeta', {}, 'GET').done(function (data) {
+                self.MetaData = data;
+                var projects = $.map(data.projects, function (item) {
+                    return {Id: item.id, Name: item.name};
+                });
+                fields.project.Options(projects);
             });
+            fields.project.Value.subscribe(function (projectId) {
+                if (projectId) {
+                    var projects = $.grep(this.MetaData.projects, function (item) {
+                        return item.id == projectId;
+                    });
+                    if (projects.length > 0) {
+                        var issuetypes = $.map(projects[0].issuetypes, function (item) {
+                            return {Id: item.id, Name: item.name};
+                        });
+                        fields.issuetype.Options(issuetypes);
+                    }
+                }
+            }, this);
             return fields;
         };
         JiraCommunicator.prototype.create = function (title, description, fields) {
-            var data = {
-                project: fields.project.Value(),
+            var data = { fields: {
+                project: {id: fields.project.Value()},
+                issuetype: {id: fields.issuetype.Value()},
                 summary: title,
                 description: description
-            };
-            return this.ajax(this.Url() + "rest/issue", data, 'PUT').then(function (location) {
-                var slashPos = location.lastIndexOf('/');
-                var id = location.substring(slashPos + 1);
-                return {Id: id};
+            }};
+            return this.ajax(this.Url() + 'rest/api/2/issue', data).then(function (data) {
+                return {Id: data.key};
             });
         };
         JiraCommunicator.prototype.attach = function (issueId, fileContent) {
@@ -62,35 +77,37 @@ define(['lib/jquery', 'comm/communicator', 'comm/fieldInfo'], function ($, Commu
             }
             var fileBlob = new Blob([new Uint8Array(arr)], {type: 'image/png'});
             var data = new FormData();
-            data.append('screenshot', fileBlob, 'screenshot.png');
-            return this.ajax(this.Url() + "rest/issue/" + issueId + "/attachment", data);
+            data.append('file', fileBlob, 'screenshot.png');
+            return this.ajax(this.Url() + 'rest/api/2/issue/' + issueId + '/attachments', data);
         };
         JiraCommunicator.prototype.comment = function (issueId, comment) {
             var data = {
-                command: 'comment',
-                comment: comment
+                body: comment
             };
-            return this.ajax(this.Url() + "rest/issue/" + issueId + "/execute", data);
+            return this.ajax(this.Url() + 'rest/api/2/issue/' + issueId + '/comment', data);
         };
         JiraCommunicator.prototype.getRedirectUrl = function (issueId, fields) {
             _super.prototype.getRedirectUrl.call(this, issueId, fields);
-            return this.Url() + 'issue/' + issueId;
+            return this.Url() + 'browse/' + issueId;
         };
         JiraCommunicator.prototype.ajax = function(url, data, method) {
             var deferred = $.Deferred();
             var xhr = new XMLHttpRequest();
             xhr.open((method || 'POST'), url, true);
+            if (data instanceof FormData) {
+                xhr.setRequestHeader('X-Atlassian-Token', 'nocheck');
+            } else {
+                xhr.setRequestHeader('Content-Type', 'application/json');
+            }
             xhr.setRequestHeader('Accept', 'application/json');
             xhr.onreadystatechange = function() {
                 if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
+                    if (xhr.status == 200 || xhr.status == 201) {
                         try {
                             deferred.resolve(JSON.parse(xhr.responseText));
                         } catch (e) {
                             deferred.resolve(xhr.responseText);
                         }
-                    } else if (xhr.status == 201) {
-                        deferred.resolve(xhr.getResponseHeader('Location'));
                     } else {
                         if(!xhr.statusText || xhr.statusText == 'timeout' || xhr.statusText == "Not Found") {
                             deferred.reject('Unable to connect to YouTrack at specified URL.');
@@ -103,7 +120,7 @@ define(['lib/jquery', 'comm/communicator', 'comm/fieldInfo'], function ($, Commu
             if (data instanceof FormData) {
                 xhr.send(data);
             } else {
-                xhr.send($.param(data));
+                xhr.send(JSON.stringify(data));
             }
             return deferred.promise();
         };
