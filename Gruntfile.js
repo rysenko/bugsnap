0e8f416f8af6337cff3eb3b488082d3e866a06de
stealth <stealth@stealthmac.local>
2013-07-21 23:43:54 +0300

    Reimplementing FireFox

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,18 +7,32 @@ module.exports = function(grunt) {
                 link: path.resolve('firefox/data')
             }
         },
-        clean: ['firefox/data'],
+        clean: ['firefox/data', 'addon-sdk-1.14'],
         zip: {
             'build/firefox.xpi': ['firefox/**'],
             'build/chrome.zip': ['chrome/**']
+        },
+        shell: {
+            unzip: {
+                 command: 'unzip addon-sdk-1.14.zip'
+            },
+            run : {
+                command: 'cd addon-sdk-1.14; source bin/activate; cd ../firefox; cfx run',
+                options: {
+                    stdout: true,
+                    stderr: true
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-symlink');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-zip');
+    grunt.loadNpmTasks('grunt-shell');
 
     grunt.registerTask('default', ['zip']);
-    grunt.registerTask('install', ['symlink']);
+    grunt.registerTask('install', ['shell:unzip', 'symlink']);
+    grunt.registerTask('run', ['shell:run']);
     grunt.registerTask('uninstall', ['clean']);
 };
