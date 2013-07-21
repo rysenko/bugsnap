236236430188580c0805d714016f174a9ec14d18
stealth <stealth@stealthmac.local>
2013-07-21 18:36:23 +0300

    Reimplementing FireFox

--- firefox/resources/bugsnap/data/content.js
+++
@@ -1,8 +0,0 @@
-bugSnapFF = {
-    start: function() {
-        self.port.on("message", function(tag) {     
-            localStorage.setItem("screenshot", tag.capture);
-        });
-    }
-};
-bugSnapFF.start();
