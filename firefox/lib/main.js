85bb509e7aa263a4ea72fc0d2983f3cc7730ff8e
stealth <stealth@stealthmac.local>
2013-07-22 08:10:11 +0300

    Working on FireFox

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -2,6 +2,21 @@ const widgets = require("sdk/widget");
 const data = require("sdk/self").data;
 var tabs = require("sdk/tabs");
 
+function openEditor(imgData) {
+    var content = "self.port.on('image', function (data) {" +
+        "  localStorage.setItem('screenshot', data);" +
+        "});";
+    tabs.open({
+        url: data.url("editor.html"),
+        onReady: function (tab) {
+            var worker = tab.attach({
+                contentScript: content
+            });
+            worker.port.emit("image", imgData);
+        }
+    });
+}
+
 var bug = widgets.Widget({
     id: 'bugsnap',
     width: 16,
@@ -9,17 +24,6 @@ var bug = widgets.Widget({
     content: '<img src="' + data.url("img/16.png") + '">',
     onClick: function() {
         var imgData = tabs.activeTab.getThumbnail();
-        var content = "self.port.on('image', function (data) {" +
-            "  localStorage.setItem('screenshot', data);" +
-            "});";
-        tabs.open({
-            url: data.url("editor.html"),
-            onReady: function (tab) {
-                var worker = tab.attach({
-                    contentScript: content
-                });
-                worker.port.emit("image", imgData);
-            }
-        });
+        openEditor(imgData);
     }
 });
